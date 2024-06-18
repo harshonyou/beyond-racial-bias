691fe735e8794b3e0fe6f5b0bfaea736eafa471d
Harsh Mohan <harshmohan813@gmail.com>
2024-06-18 07:01:09 +0100

    feat: update fitting proc
    this still contains unwanted LOC

--- demos/custom.py
+++ demos/custom.py
@@ -9,22 +9,23 @@ import datetime
 import distutils.version
 from torch.utils.tensorboard import SummaryWriter
 import matplotlib.pyplot as plt
+from PIL import Image, ImageDraw, ImageFont
+
 sys.path.append('.')
 from models.FLAME import FLAME, TextureModel, FLAMETex
 from models.face_seg_model import BiSeNet
 from models.RENI import RENI
 from utils.renderer import Renderer
 from utils import util
+from utils.lr import CustomExponentialLR, CustomGroupLR
 from utils.config import cfg
 from facial_alignment.detection import sfd_detector as detector
 from facial_alignment.detection import FAN_landmark
-
 
 from reni_plus_plus.model_components.illumination_samplers import EquirectangularSamplerConfig
 from reni_plus_plus.field_components.field_heads import RENIFieldHeadNames
 
 torch.backends.cudnn.benchmark = True
-
 torch.autograd.set_detect_anomaly(True)
 
 class PhotometricFitting(object):
@@ -58,17 +59,69 @@ class PhotometricFitting(object):
         cam[:, 0] = 5.
         cam = nn.Parameter(cam.float().to(self.device))
 
-        latent_codes = nn.Parameter(torch.zeros(bz, cfg.reni_latent_dim_size, 3).float().to(self.device))
+        # latent_codes = nn.Parameter(torch.zeros(bz, cfg.reni_latent_dim_size, 3).float().to(self.device))
         scale = nn.Parameter(torch.zeros(bz).float().to(self.device))
 
-        # lights = {'latent_codes': latent_codes, 'scale': scale}
+        latent_codes = nn.Parameter(torch.zeros(bz, cfg.reni_latent_dim_size, 3).float().to(self.device))
 
+        # mask = latent_codes > 0.75
+        # latent_codes.data[mask] = 1
+        # mask = latent_codes < 0.25
+        # latent_codes.data[mask] = 0
 
-        e_opt = torch.optim.Adam(
-            [shape, exp, pose, cam, tex, latent_codes, scale],
-            lr=cfg.e_lr,
-            weight_decay=cfg.e_wd
-        )
+        # e_opt = torch.optim.Adam(
+        #     [shape, exp, pose, cam, tex, latent_codes, scale],
+        #     lr=cfg.e_lr,
+        #     weight_decay=cfg.e_wd
+        # )
+        # Configuration for learning rates
+        # albedo_lr = cfg.e_lr * 0.25  # Assuming you want to increase the learning rate for albedo by 10x
+        albedo_lr = cfg.e_lr * 1 #* 0.25 # 0.125  # 18 I
+        default_lr = cfg.e_lr * 1.5  # Original learning rate for other parameters
+        latent_lr = cfg.e_lr * 0.125 # 0.025 #0.01  # Half the original learning rate for latent codes
+
+        e_opt = torch.optim.Adam([
+            {'params': [shape, exp, cam], 'lr': default_lr, 'weight_decay': cfg.e_wd, 'initial_lr': default_lr},
+            {'params': [pose, tex], 'lr': albedo_lr, 'weight_decay': cfg.e_wd, 'initial_lr': albedo_lr},  # Higher learning rate for albedo
+            {'params': [scale, latent_codes], 'lr': latent_lr, 'weight_decay': cfg.e_wd, 'initial_lr': latent_lr}  # Lower learning rate for latent codes
+        ])
+
+        # s_opt = torch.optim.lr_scheduler.ExponentialLR(e_opt, gamma=0.95)
+        # Define custom decay rates for each group
+        # decay_rates = [0.99, 0.97, 0.95]  # 6 VI, 30ish I
+        # intervals = [100, 250, 50]
+
+        # 26 I
+        # decay_rates = [0.99, 0.90, 0.95]  # For example, slower decay for tex, faster for latent_codes
+        # intervals = [100, 250, 50]
+
+        decay_rates = [1, 1, 1]  # For example, slower decay for tex, faster for latent_codes
+        intervals = [100, 100, 100]
+
+        rigid_mode = True
+
+        # Example of phase settings
+        # phase_settings = [
+        #     (500, [1.0,     0.0,    0.0]),  # Phase 1: High LR for albedo, low for illumination
+        #     (500, [0.25,    1.0,    0.1]),  # Phase 2: Low LR for albedo, high for illumination
+        #     (250, [0.25,    0.1,    1.0]),  # Phase 3: Repeat Phase 1 settings
+        #     (250, [0.25,    1.0,    0.1]),  # Phase 4: Repeat Phase 2 settings
+        #     (500, [0.125,   0.5,    0.5])  # Final Phase: High LR for both
+        # ]
+        phase_settings = [
+            (500, [1.0,     0.0,    0.0]),  # Phase 1: High LR for albedo, low for illumination
+            (250, [0.0,     1.0,    0.0]),  # Phase 2: Low LR for albedo, high for illumination
+            (1500, [1.0,     1.0,    1.0]),  # Phase 2: Low LR for albedo, high for illumination
+        ]
+
+        # phase_settings = [
+        #     (1000, [1.0,     1.0,    1.0]),  # Phase 1: High LR for albedo, low for illumination
+        # ]
+
+        # Create the custom scheduler
+        # s_opt = CustomExponentialLR(e_opt, decay_rates)
+        # s_opt = CustomGroupLR(e_opt, decay_rates, intervals)
+        s_opt = CustomGroupLR(e_opt, phase_settings)
 
         gt_landmark = landmarks
 
@@ -94,10 +147,8 @@ class PhotometricFitting(object):
         # del albedo
 
 
-        for k in range(250):
+        for k in range(cfg.max_iter + 500): # cfg.max_iter
             losses = {}
-
-            predicted_illumination = self.reni(rotation=None, latent_codes=latent_codes, scale=scale)
 
             vertices, landmarks2d, landmarks3d = self.flame(shape_params=shape, expression_params=exp, pose_params=pose)
             trans_vertices = util.batch_orth_proj(vertices, cam)
@@ -114,15 +165,21 @@ class PhotometricFitting(object):
             # losses['tex_reg'] = (torch.sum(tex ** 2) / 2) * 1e-4
             # losses['latent_code_reg'] = (torch.sum(latent_codes ** 2) / 2) * 1e-4
 
-            # render
-            albedos = self.flametex(tex) / 255.
-            ops = self.render(vertices, trans_vertices, albedos, illumination=predicted_illumination)
-            predicted_images = ops['images']
-            # losses['photometric_texture'] = (image_masks * (ops['images'] - images).abs()).mean() * config.w_pho
-            losses['photometric_texture'] = F.smooth_l1_loss(image_masks * ops['images'],
-                                                             image_masks * images) * cfg.w_pho
+            if rigid_mode:
+                losses['photometric_texture'] = torch.tensor(0.0)  # Set as a zero tensor
+                losses['exponential'] = torch.tensor(0.0)  # Set as a zero tensor
+            else:
+                predicted_illumination = self.reni(rotation=None, latent_codes=latent_codes, scale=scale)
+                # render
+                albedos = self.flametex(tex) / 255.
+                ops = self.render(vertices, trans_vertices, albedos, illumination=predicted_illumination)
+                predicted_images = ops['images']
+                # losses['photometric_texture'] = (image_masks * (ops['images'] - images).abs()).mean() * config.w_pho
 
-            losses['exponential'] = latent_codes.pow(2).mean() * 1e-4
+                losses['photometric_texture'] = F.smooth_l1_loss(image_masks * ops['images'],
+                                                                image_masks * images) * cfg.w_pho
+
+                losses['exponential'] = latent_codes.pow(2).mean() * 1e-4 * 10
 
             all_loss = 0.
             for key in losses.keys():
@@ -131,6 +188,9 @@ class PhotometricFitting(object):
             e_opt.zero_grad()
             all_loss.backward()
             e_opt.step()
+            s_opt.step()
+            # if k % 100 == 0:
+            #     s_opt.step()
 
             loss_info = '----iter: {}, time: {}\n'.format(k, datetime.datetime.now().strftime('%Y-%m-%d-%H:%M:%S'))
             for key in losses.keys():
@@ -140,7 +200,14 @@ class PhotometricFitting(object):
             #     print(loss_info)
 
 
-            if k % 10 == 0:
+            if (k+1) % 50 == 0:
+                if not rigid_mode:
+                    # Get gradients (as a measure of contribution)
+                    tex_grad = torch.norm(tex.grad).item()
+                    latent_codes_grad = torch.norm(latent_codes.grad).item()
+                    self.writer.add_scalar('Gradient/tex', tex_grad, k)
+                    self.writer.add_scalar('Gradient/latent_codes', latent_codes_grad, k)
+
                 all_train_iter += 10
                 all_train_iters.append(all_train_iter)
                 photometric_loss.append(losses['photometric_texture'])
@@ -158,6 +225,7 @@ class PhotometricFitting(object):
                 self.writer.add_scalar('Loss/expression_reg', losses['expression_reg'].item(), k)
                 self.writer.add_scalar('Loss/pose_reg', losses['pose_reg'].item(), k)
                 self.writer.add_scalar('Loss/photometric_texture', losses['photometric_texture'].item(), k)
+                self.writer.add_scalar('Loss/exponential', losses['exponential'].item(), k)
                 self.writer.add_scalar('Loss/all_loss', all_loss.item(), k)
 
                 self.writer.add_histogram('Parameters/shape', shape, k)
@@ -168,33 +236,59 @@ class PhotometricFitting(object):
                 self.writer.add_histogram('Parameters/latent_codes', latent_codes, k)
                 self.writer.add_histogram('Parameters/scale', scale, k)
 
+                # Log learning rates
+                for i, group in enumerate(e_opt.param_groups):
+                    self.writer.add_scalar(f'Learning Rate/Group {i+1}', group['lr'], k)
+
                 print(loss_info)
 
-                grids = {}
-                visind = range(bz)  # [0]
-                grids['images'] = torchvision.utils.make_grid(images[visind]).detach().cpu()
-                grids['landmarks_gt'] = torchvision.utils.make_grid(
-                    util.tensor_vis_landmarks(images[visind], landmarks[visind]))
-                grids['landmarks2d'] = torchvision.utils.make_grid(
-                    util.tensor_vis_landmarks(images[visind], landmarks2d[visind]))
-                grids['landmarks3d'] = torchvision.utils.make_grid(
-                    util.tensor_vis_landmarks(images[visind], landmarks3d[visind]))
-                grids['albedoimage'] = torchvision.utils.make_grid(
-                    (ops['albedo_images'])[visind].detach().cpu())
-                illumination_image = F.interpolate(self.reni.visualize_illumination(predicted_illumination).permute(2, 0, 1).unsqueeze(0), size=(224, 224), mode='bilinear', align_corners=False).squeeze(0)
-                grids['illumination'] = torchvision.utils.make_grid(
-                    (illumination_image).detach().cpu())
-                grids['render'] = torchvision.utils.make_grid(predicted_images[visind].detach().float().cpu())
-                shape_images = self.render.render_shape(vertices, trans_vertices, images)
-                grids['shape'] = torchvision.utils.make_grid(
-                    F.interpolate(shape_images[visind], [224, 224])).detach().float().cpu()
+                if not rigid_mode:
+                    grids = {}
+                    visind = range(bz)  # [0]
+                    grids['images'] = torchvision.utils.make_grid(images[visind]).detach().cpu()
+                    grids['landmarks_gt'] = torchvision.utils.make_grid(
+                        util.tensor_vis_landmarks(images[visind], landmarks[visind]))
+                    grids['landmarks2d'] = torchvision.utils.make_grid(
+                        util.tensor_vis_landmarks(images[visind], landmarks2d[visind]))
+                    grids['landmarks3d'] = torchvision.utils.make_grid(
+                        util.tensor_vis_landmarks(images[visind], landmarks3d[visind]))
+                    grids['albedoimage'] = torchvision.utils.make_grid(
+                        (ops['albedo_images'])[visind].detach().cpu())
+                    illumination_image = F.interpolate(self.reni.visualize_illumination(predicted_illumination).permute(2, 0, 1).unsqueeze(0), size=(224, 224), mode='bilinear', align_corners=False).squeeze(0)
+                    grids['illumination'] = torchvision.utils.make_grid(
+                        (illumination_image).detach().cpu())
+                    grids['render'] = torchvision.utils.make_grid(predicted_images[visind].detach().float().cpu())
+                    shape_images = self.render.render_shape(vertices, trans_vertices, images)
+                    grids['shape'] = torchvision.utils.make_grid(
+                        F.interpolate(shape_images[visind], [224, 224])).detach().float().cpu()
 
-                # grids['tex'] = torchvision.utils.make_grid(F.interpolate(albedos[visind], [224, 224])).detach().cpu()
-                del illumination_image, shape_images
-                grid = torch.cat(list(grids.values()), 1)
-                grid_image = (grid.numpy().transpose(1, 2, 0).copy() * 255)[:, :, [2, 1, 0]]
-                grid_image = np.minimum(np.maximum(grid_image, 0), 255).astype(np.uint8)
-                video_writer.write(grid_image)
+                    # grids['tex'] = torchvision.utils.make_grid(F.interpolate(albedos[visind], [224, 224])).detach().cpu()
+                    del illumination_image, shape_images
+                    grid = torch.cat(list(grids.values()), 1)
+                    grid_image = (grid.numpy().transpose(1, 2, 0).copy() * 255)[:, :, [2, 1, 0]]
+                    grid_image = np.minimum(np.maximum(grid_image, 0), 255).astype(np.uint8)
+
+                    # Convert numpy array to PIL Image for drawing text
+                    pil_image = Image.fromarray(grid_image)
+
+                    # Prepare to draw the iteration number
+                    draw = ImageDraw.Draw(pil_image)
+                    font = ImageFont.load_default() #= ImageFont.truetype("arial.ttf", 16)  # Specify the font and size you want
+                    text = f"Iteration: {k}"
+
+                    # Specify the position where you want the text
+                    position = (10, 224*7 + 50)  # Change this as needed
+
+                    # Draw text on the image
+                    draw.text(position, text, font=font, fill=(255, 255, 255))  # White text or choose your color
+
+                    # Convert back to numpy array if needed for video writing
+                    grid_image = np.array(pil_image)
+
+                    video_writer.write(grid_image)
+
+                if k >= 500:
+                    rigid_mode = False
 
         single_params = {
             'shape': shape.detach().cpu().numpy(),
@@ -272,6 +366,8 @@ if __name__ == '__main__':
     util.check_mkdir(cfg.save_folder)
     fitting = PhotometricFitting(device=device_name)
     img = cv2.imread(image_path)
+    # padding_size = 800
+    # img_padded = cv2.copyMakeBorder(img, padding_size, padding_size, padding_size, padding_size, cv2.BORDER_CONSTANT, value=0)
 
     face_detect = detector.SFDDetector(device_name, cfg.rect_model_path)
     face_landmark = FAN_landmark.FANLandmarks(device_name, cfg.landmark_model_path, cfg.face_detect_type)
